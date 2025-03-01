5a76ff393694be11d98759cf85f39ef0de5153aa
Udayan Sawant <udayansawant7@gmail.com>
2025-02-28 21:01:04 -0800

    Feature 1 : Multi Agent Conversation - Conversable Agent & Converation

--- utils.py
+++ utils.py
@@ -1,13 +1,18 @@
-# Add your utilities or helper functions to this file.
-
 import os
 from dotenv import load_dotenv, find_dotenv
 
-# these expect to find a .env file at the directory above the lesson.                                                                                                                     # the format for that file is (without the comment)                                                                                                                                       #API_KEYNAME=AStringThatIsTheLongAPIKeyFromSomeService                                                                                                                                     
+# Function to load environment variables from a .env file
 def load_env():
     _ = load_dotenv(find_dotenv())
 
+# Function to retrieve the OpenAI API key from environment variables
 def get_openai_api_key():
     load_env()
     openai_api_key = os.getenv("OPENAI_API_KEY")
+    
+    if not openai_api_key:
+        raise ValueError("OpenAI API key not found. Make sure it's set in the .env file.")
+    
+    print("API Key Loaded:", openai_api_key)
+    
     return openai_api_key
